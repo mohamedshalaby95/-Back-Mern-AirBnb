d4ee3de5f131d9c002a6b2dff721e7241e33fcd3
mohamedshalaby95 <mohamedshalaby19595@gmail.com>
2021-02-22 09:05:26 -0800

    url base update

--- index.js
+++ index.js
@@ -12,7 +12,14 @@ const reservation = require('./src/reservation/reservation-routes')
 const app = express()
 app.use(bodyParser.json())
 app.use(helmet())
-app.use(cors())
+app.use(
+    cors({
+        origin: process.env.CLIENT_BASE_URL,
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        credentials: true,
+    }),
+)
+
 
 app.use('/user', user)
 app.use('/reservation', reservation)
